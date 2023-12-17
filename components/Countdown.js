96876f500525fe8f1f3266d6cc6c5d48ca9d5c33
LoLoSenPai <loic.dlugosz@gmail.com>
2023-12-18 00:43:34 +0100

    Update countdown date

--- components/Countdown.js
+++ components/Countdown.js
@@ -10,7 +10,7 @@ const silkscreen = Silkscreen({
 
 export default function Home() {
     const calculateTimeLeft = () => {
-        const difference = +new Date("12/20/2023") - +new Date();
+        const difference = +new Date("12/27/2023") - +new Date();
         let timeLeft = {};
 
         if (difference > 0) {
